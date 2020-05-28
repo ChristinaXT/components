5d9068434b287aa628fa4333d6a3ddc30384f92d
Christina Williams <christinaxtwilliams@gmail.com>
2020-05-28 14:02:49 -0400

    props

--- src/index.js
+++ src/index.js
@@ -6,9 +6,24 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:30PM"
+        content="Nice Blog Post!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 5:00PM"
+        content="I like the writing"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 6:15PM"
+        content="Nice one"
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
